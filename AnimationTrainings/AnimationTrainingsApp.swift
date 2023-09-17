c150c1ea2a224e9765516a4defa63882c0e0fa58
Cihat Tasci <cihattasci97@gmail.com>
2023-09-17 11:30:33 +0300

    Initial Commit

---
+++ AnimationTrainings/AnimationTrainingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationTrainingsApp.swift
+//  AnimationTrainings
+//
+//  Created by Cihat Tascı on 17.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimationTrainingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
